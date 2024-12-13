4541b4189a2d45aad1d51c887b218a73ef3ff04b
Robinson Lourenço <robinson.lourencoti@gmail.com>
2024-12-13 15:22:10 -0300

    Switch to System.Text.Json for JSON operations
    Replaced Newtonsoft.Json with System.Text.Json for serialization and deserialization in the AlertaDengueAPI class. Updated JsonProperty attributes to JsonPropertyName. Removed unused, commented-out properties.

--- InfoDengue/Domain/Models/AlertaDengueAPI.cs
+++ InfoDengue/Domain/Models/AlertaDengueAPI.cs
@@ -1,127 +1,97 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
 
 namespace InfoDengue.Domain.Models
 {
     public class AlertaDengueAPI
     {
-        [JsonProperty("data_iniSE")]
+        [JsonPropertyName("data_iniSE")]
         public DateTime DataIniSE { get; set; }
 
-        [JsonProperty("SE")]
+        [JsonPropertyName("SE")]
         public int SE { get; set; }
 
-        [JsonProperty("casos_est")]
+        [JsonPropertyName("casos_est")]
         public double CasosEst { get; set; }
 
-        [JsonProperty("casos_est_min")]
+        [JsonPropertyName("casos_est_min")]
         public int CasosEstMin { get; set; }
 
-        [JsonProperty("casos_est_max")]
+        [JsonPropertyName("casos_est_max")]
         public int? CasosEstMax { get; set; }
 
-        [JsonProperty("casos")]
+        [JsonPropertyName("casos")]
         public int Casos { get; set; }
 
-        [JsonProperty("p_rt1")]
+        [JsonPropertyName("p_rt1")]
         public double PRt1 { get; set; }
 
-        [JsonProperty("p_inc100k")]
+        [JsonPropertyName("p_inc100k")]
         public double PInc100k { get; set; }
 
-        [JsonProperty("Localidade_id")]
+        [JsonPropertyName("Localidade_id")]
         public int LocalidadeId { get; set; }
 
-        [JsonProperty("nivel")]
+        [JsonPropertyName("nivel")]
         public int Nivel { get; set; }
 
-        [JsonProperty("id")]
+        [JsonPropertyName("id")]
         public long Id { get; set; }
 
-        [JsonProperty("versao_modelo")]
+        [JsonPropertyName("versao_modelo")]
         public DateTime VersaoModelo { get; set; }
 
-        [JsonProperty("tweet")]
+        [JsonPropertyName("tweet")]
         public long Tweet { get; set; }
 
-        [JsonProperty("Rt")]
+        [JsonPropertyName("Rt")]
         public double Rt { get; set; }
 
-        [JsonProperty("pop")]
+        [JsonPropertyName("pop")]
         public long Pop { get; set; }
 
-        [JsonProperty("tempmin")]
+        [JsonPropertyName("tempmin")]
         public double Tempmin { get; set; }
 
-        [JsonProperty("umidmax")]
+        [JsonPropertyName("umidmax")]
         public double Umidmax { get; set; }
 
-        [JsonProperty("receptivo")]
+        [JsonPropertyName("receptivo")]
         public int Receptivo { get; set; }
 
-        [JsonProperty("transmissao")]
+        [JsonPropertyName("transmissao")]
         public int Transmissao { get; set; }
 
-        [JsonProperty("nivel_inc")]
+        [JsonPropertyName("nivel_inc")]
         public int NivelInc { get; set; }
 
-        [JsonProperty("umidmed")]
+        [JsonPropertyName("umidmed")]
         public double Umidmed { get; set; }
 
-        [JsonProperty("umidmin")]
+        [JsonPropertyName("umidmin")]
         public double Umidmin { get; set; }
 
-        [JsonProperty("tempmed")]
+        [JsonPropertyName("tempmed")]
         public double Tempmed { get; set; }
 
-        [JsonProperty("tempmax")]
+        [JsonPropertyName("tempmax")]
         public double Tempmax { get; set; }
 
-        [JsonProperty("casprov")]
+        [JsonPropertyName("casprov")]
         public int Casprov { get; set; }
 
-        [JsonProperty("casprov_est")]
+        [JsonPropertyName("casprov_est")]
         public double CasprovEst { get; set; }
 
-        [JsonProperty("casprov_est_min")]
+        [JsonPropertyName("casprov_est_min")]
         public double CasprovEstMin { get; set; }
 
-        [JsonProperty("casprov_est_max")]
+        [JsonPropertyName("casprov_est_max")]
         public double CasprovEstMax { get; set; }
 
-        [JsonProperty("casconf")]
+        [JsonPropertyName("casconf")]
         public double Casconf { get; set; }
 
-        [JsonProperty("notif_accum_year")]
+        [JsonPropertyName("notif_accum_year")]
         public int NotifAccumYear { get; set; }
-        //public DateTime data_iniSE { get; set; }
-        //public int SE { get; set; }
-        //public double casos_est { get; set; }
-        //public int casos_est_min { get; set; }
-        //public object casos_est_max { get; set; }
-        //public int casos { get; set; }
-        //public double p_rt1 { get; set; }
-        //public double p_inc100k { get; set; }
-        //public int Localidade_id { get; set; }
-        //public int nivel { get; set; }
-        //public object id { get; set; }
-        //public string versao_modelo { get; set; }
-        //public object tweet { get; set; }
-        //public double Rt { get; set; }
-        //public double pop { get; set; }
-        //public double tempmin { get; set; }
-        //public double umidmax { get; set; }
-        //public int receptivo { get; set; }
-        //public int transmissao { get; set; }
-        //public int nivel_inc { get; set; }
-        //public double umidmed { get; set; }
-        //public double umidmin { get; set; }
-        //public double tempmed { get; set; }
-        //public double tempmax { get; set; }
-        //public int casprov { get; set; }
-        //public object casprov_est { get; set; }
-        //public object casprov_est_min { get; set; }
-        //public object casprov_est_max { get; set; }
-        //public object casconf { get; set; }
-        //public int notif_accum_year { get; set; }
     }
 }
